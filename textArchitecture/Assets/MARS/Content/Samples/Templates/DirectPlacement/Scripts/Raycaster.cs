3cb5fe410935a7bd4ac8f9692d078e4029a03ae8
jieguann <60665347+jieguann@users.noreply.github.com>
2021-07-17 15:05:14 -0400

    Add Unity File

---
+++ textArchitecture/Assets/MARS/Content/Samples/Templates/DirectPlacement/Scripts/Raycaster.cs
@@ -0,0 +1,205 @@
+﻿using Unity.MARS.MARSUtils;
+using UnityEngine;
+
+namespace Unity.MARS.Content
+{
+    /// <summary>
+    /// Raycasts against colliders and specialized placement objects
+    /// Allows objects to be placed in the given location
+    /// </summary>
+    public class Raycaster : MonoBehaviour
+    {
+        static RaycastHit[] s_RaycastResults = new RaycastHit[10];
+
+        [SerializeField]
+        [Tooltip("True drives this raycaster internally via mouse/tap events. Set to false to drive this raycaster via a parent transform and events.")]
+        bool m_ScreenMode = true;
+
+        [SerializeField]
+        [Tooltip("Gameobject that is set to the hit location of the raycast. Disabled when no target is available.")]
+        GameObject m_Cursor;
+
+        [SerializeField]
+        [Tooltip("The object to place.")]
+        GameObject m_ObjectToPlace;
+
+        [SerializeField]
+        [Tooltip("Collider mask for raycast targets.")]
+        LayerMask m_Mask = 1;
+
+        [SerializeField]
+        [Tooltip("Only allow raycasts against objects with PlacementTarget scripts.")]
+        bool m_PlacementTargetsOnly = false;
+
+        [SerializeField]
+        [Tooltip("Maximum distance to check for raycasting.")]
+        float m_MaskDistance = 10.0f;
+
+        [SerializeField]
+        [Tooltip("If the cursor should align to the raycast surface normal.")]
+        bool m_AlignToTargetNormal = false;
+
+        [SerializeField]
+        [Tooltip("In screen mode, how long input must be pressed before the object is placed.")]
+        float m_PlacementHoldTime = 1.0f;
+
+        Vector3 m_ScreenPoint = new Vector3(0.5f, 0.5f, 0.0f);
+        Camera m_Camera;
+
+        Transform m_CursorTransform;
+        Transform m_CameraTransform;
+
+        bool m_ValidTarget = false;
+        float m_PlacementTimer = 0.0f;
+
+        PlacementTarget m_LastTarget;
+
+        void Start()
+        {
+            m_Camera = MarsRuntimeUtils.GetActiveCamera(true);
+            m_CameraTransform = m_Camera.transform;
+
+            if (m_Cursor != null)
+            {
+                m_CursorTransform = m_Cursor.transform;
+                m_Cursor.SetActive(false);
+            }
+
+            if (m_ObjectToPlace != null)
+                m_ObjectToPlace.SetActive(false);
+        }
+
+        void Update()
+        {
+            if (m_ObjectToPlace == null)
+            {
+                DisableCursor();
+                return;
+            }
+
+            // If the screen has been tapped, update where the cursor is pointing
+            if (Input.GetMouseButtonDown(0))
+                m_ScreenPoint = Vector3.Scale(Input.mousePosition, new Vector3(1.0f / Screen.width, 1.0f / Screen.height, 0.0f));
+
+            // Make a ray - set to the last touched screen position in screen mode. Otherwise just mirror the camera
+            var screenRay = new Ray(m_CameraTransform.position, m_CameraTransform.forward);
+
+            if (m_ScreenMode)
+            {
+                var screenCoords = Vector3.Scale(new Vector3(Screen.width, Screen.height, 0.0f), m_ScreenPoint);
+                screenRay = m_Camera.ScreenPointToRay(screenCoords, Camera.MonoOrStereoscopicEye.Mono);
+            }
+
+            // Use this ray to find any potential colliders
+            var foundHits = Physics.RaycastNonAlloc(screenRay, s_RaycastResults, m_MaskDistance, m_Mask);
+            var rayHits = foundHits;
+
+            // Filter out non-placement targets if the user has requested it
+            var rayCounter = 0;
+            if (m_PlacementTargetsOnly)
+            {
+                while (rayCounter < foundHits)
+                {
+                    if (s_RaycastResults[rayCounter].collider.GetComponent<PlacementTarget>() == null)
+                    {
+                        s_RaycastResults[rayCounter].distance = m_MaskDistance + 1.0f;
+                        rayHits--;
+                    }
+                    rayCounter++;
+                }
+            }
+
+            // No hits? Turn off the target
+            if (rayHits <= 0)
+            {
+                DisableCursor();
+                return;
+            }
+            else
+                m_Cursor.SetActive(true);
+
+            m_ValidTarget = true;
+
+            // Find the closest target and align the cursor to that target
+            var closestHit = 0;
+            var closestHitDistance = m_MaskDistance;
+            rayCounter = 0;
+            while (rayCounter < foundHits)
+            {
+                if (s_RaycastResults[rayCounter].distance < closestHitDistance)
+                {
+                    closestHit = rayCounter;
+                    closestHitDistance = s_RaycastResults[rayCounter].distance;
+                }
+                rayCounter++;
+            }
+
+            var closestPlacementTarget = s_RaycastResults[closestHit].collider.GetComponent<PlacementTarget>();
+            if (closestPlacementTarget != null && closestPlacementTarget != m_LastTarget)
+                closestPlacementTarget.HoverBegin();
+
+            if (m_LastTarget != closestPlacementTarget && m_LastTarget != null)
+                m_LastTarget.HoverEnd();
+
+            m_LastTarget = closestPlacementTarget;
+
+            // Align the cursor to this target
+            m_CursorTransform.position = s_RaycastResults[closestHit].point;
+            var normal = m_AlignToTargetNormal ? s_RaycastResults[closestHit].normal : Vector3.up;
+            var forward = m_CameraTransform.forward;
+            if (Mathf.Abs(Vector3.Dot(normal, forward)) > 0.5f)
+                forward = m_CameraTransform.up;
+
+            m_CursorTransform.rotation = Quaternion.LookRotation(Vector3.Cross(Vector3.Cross(forward, normal), normal), normal);
+
+            // If the touch is held down, trigger a placement
+            m_PlacementTimer = Input.GetMouseButton(0) ? m_PlacementTimer + Time.deltaTime : 0.0f;
+
+            if (m_PlacementTimer >= m_PlacementHoldTime)
+                PlaceObject();
+        }
+
+        void DisableCursor()
+        {
+            m_ValidTarget = false;
+            m_PlacementTimer = 0.0f;
+            m_Cursor.SetActive(false);
+        }
+
+        /// <summary>
+        /// Places the loaded object at the cursor's location
+        /// Call this method manually if driving via XRI or other non-screen based approaches
+        /// </summary>
+        public void PlaceObject()
+        {
+            // Can't place if we don't have a valid target
+            if (!m_ValidTarget)
+                return;
+
+            // No cursor needed after the object is placed
+            DisableCursor();
+
+            // Can't place an object we do not have!
+            if (m_ObjectToPlace == null)
+                return;
+
+            var objectTransform = m_ObjectToPlace.transform;
+            objectTransform.parent = null;
+            objectTransform.position = m_CursorTransform.position;
+            objectTransform.rotation = m_CursorTransform.rotation;
+            m_ObjectToPlace.SetActive(true);
+            m_ObjectToPlace = null;
+        }
+
+        /// <summary>
+        /// Sets the object that will be placed when the user holds down the input
+        /// </summary>
+        /// <param name="toPlace">The object to place</param>
+        public void SetObjectToPlace(GameObject toPlace)
+        {
+            if (m_ObjectToPlace != null)
+                m_ObjectToPlace.SetActive(false);
+        }
+    }
+}
+
---
+++ textArchitecture/Assets/textArchitecture/textVisulization.cs
@@ -0,0 +1,230 @@
+
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using SimpleJSON;
+using CI.HttpClient;
+using System.Collections;
+
+public class textVisulization : MonoBehaviour
+{
+    public GameObject myPrefab;
+    GameObject[] textObjects;
+    public int resolution;
+    
+    //public TextAsset jsonFile;
+    //public string[] jsonArray;
+    //public ListItem items;
+    JSONNode items;
+    //int index;
+    public float architectureScale;
+    public Slider ArchSliderScale;
+    public float textScale;
+    public Slider textSliderScale;
+    //text;
+    TextMeshPro textmeshPro;
+    public Vector3[] prefebPosition;
+    public Quaternion[] prefebRotation;
+    public Vector3[] prefebScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ArchSliderScale.value = 0.5f;
+        textScale = 1f;
+        //Debug.Log(items["textDatas"]);
+        //StartCoroutine(GetText());
+        StartCoroutine(ReadJsonHTTP());
+
+
+
+        //Debug.Log(items["textDatas"]);
+        //items = JsonUtility.FromJson<ListItem>(jsonFile.text);
+        //print(json.items);
+        textObjects = new GameObject[resolution];
+        prefebPosition = new Vector3[resolution];
+        prefebRotation = new Quaternion[resolution];
+        prefebScale = new Vector3[resolution];
+        for (int i = 0; i < resolution; i++)
+        {
+            GameObject textObject = textObjects[i] = Instantiate(myPrefab);
+            textObjects[i].GetComponent<Transform>().SetParent(transform);
+            prefebPosition[i] = new Vector3(0, 0, 0) * architectureScale;
+            prefebRotation[i] = Quaternion.Euler(0,0,0);
+            prefebScale[i] = new Vector3(0, 0, 0) * textScale;
+            //prefebPosition = new Vector3[i](0, Random.Range(0f,1f), 0) * architectureScale;
+
+            //prefebPosition[i]= new Vector3(0, Random.Range(0f, 1f), 0) * architectureScale;
+            //prefebPosition[i] = new Vector3(0, Random.Range(0f, 1f), 0) * architectureScale;
+            //Instantiate(myPrefab);
+        }
+
+
+            /*
+            textObjects = new GameObject[items.textDatas.Length];
+            for (int i = 0; i < items.textDatas.Length; i++)
+            {
+                //Debug.Log(items.textDatas[i].Input);
+                //Vector3 prefebPosition = new Vector3(items.textDatas[i].X, items.textDatas[i].Y, items.textDatas[i].Z) * architectureScale;
+                //Quaternion prefebRotation = Quaternion.Euler(items.textDatas[i].rX, items.textDatas[i].rY, items.textDatas[i].rZ);
+                //Vector3 prefebScale = new Vector3(items.textDatas[i].sX, items.textDatas[i].sY, items.textDatas[i].sZ);
+
+
+                GameObject textObject = textObjects[i] = Instantiate(myPrefab);
+                textObjects[i].GetComponent<Transform>().SetParent(transform);
+                //textObjects[i].GetComponent<Transform>().localPosition = prefebPosition;
+                //textObjects[i].GetComponent<Transform>().localRotation = prefebRotation;
+                //textObjects[i].GetComponent<Transform>().localScale = prefebScale;
+                //Instantiate(myPrefab, prefebPosition, prefebRotation);
+
+                
+                textmeshPro.SetText(items.textDatas[i].Input);
+
+                if (items.textDatas[i].Color == 4)
+                {
+                    textmeshPro.color = new Color32(245, 215, 66, 255);
+                }
+                else if (items.textDatas[i].Color == 3)
+                {
+                    textmeshPro.color = new Color32(240, 153, 72, 255);
+                }
+                else if (items.textDatas[i].Color == 2)
+                {
+                    textmeshPro.color = new Color32(167, 222, 95, 255);
+                }
+                else if (items.textDatas[i].Color == 1)
+                {
+                    textmeshPro.color = new Color32(255, 82, 82, 255);
+                }
+                else if (items.textDatas[i].Color == 0)
+                {
+                    textmeshPro.color = new Color32(115, 47, 47, 255);
+                }
+
+
+
+
+        }
+             */
+
+        }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        textVisualizer();
+        ReadJsonHTTP();
+        architectureScale = ArchSliderScale.value;
+        textScale = textSliderScale.value;
+
+    }
+
+    IEnumerator ReadJsonHTTP()
+    {
+        while (true) {
+            var client = new HttpClient();
+
+            // Send the GET request to the specified uri
+            client.Get(new System.Uri("http://68.183.206.206:8000/text.json"), HttpCompletionOption.AllResponseContent, r =>
+            {
+                // This callback is raised when the request completes
+                if (r.IsSuccessStatusCode)
+                {
+                    // Read the response content as a string if the server returned a success status code
+                    items = JSON.Parse(r.ReadAsString());
+                    //print(items["textDatas"]);
+
+                }
+
+            });
+            yield return new WaitForSeconds(0.5f);
+        }
+
+       
+    }
+
+    void textVisualizer()
+    {
+        if (items != null)
+        {
+
+            //print(items["textDatas"]);
+            for (int i = 0; i < resolution; i++)
+            {
+                textmeshPro = textObjects[i].GetComponent<TextMeshPro>();
+                if (items["textDatas"][i] != null)
+                {
+                    //print(items["textDatas"][0]["Input"]);
+                    //textmeshPro.SetText(items["textDatas"][i]["Input"]);
+                    textmeshPro.SetText(items["textDatas"][i]["Input"]);
+                    prefebPosition[i] = new Vector3(items["textDatas"][i]["X"], items["textDatas"][i]["Y"], items["textDatas"][i]["Z"]) * architectureScale;
+                    prefebRotation[i] = Quaternion.Euler(0, items["textDatas"][i]["rY"] * 360f, 0);
+                    prefebScale[i] = new Vector3(0.01f, 0.01f, 0.01f) * textScale;
+                    //print(items["textDatas"][resolution]["Input"]);
+
+
+                }
+                else
+                {
+                    textmeshPro.SetText("");
+                }
+
+                //textObjects[i].GetComponent<Transform>().localPosition = prefebPosition[i];
+                textObjects[i].GetComponent<Transform>().localPosition = prefebPosition[i];
+                textObjects[i].GetComponent<Transform>().localRotation = prefebRotation[i];
+                textObjects[i].GetComponent<Transform>().localScale = prefebScale[i];
+
+                if (items["textDatas"][i]["Mood"] == 4)
+                {
+                    textmeshPro.color = new Color32(245, 215, 66, 255);
+                }
+                else if (items["textDatas"][i]["Mood"] == 3)
+                {
+                    textmeshPro.color = new Color32(240, 153, 72, 255);
+                }
+                else if (items["textDatas"][i]["Mood"] == 2)
+                {
+                    textmeshPro.color = new Color32(167, 222, 95, 255);
+                }
+                else if (items["textDatas"][i]["Mood"] == 1)
+                {
+                    textmeshPro.color = new Color32(255, 82, 82, 255);
+                }
+                else if (items["textDatas"][i]["Mood"] == 0)
+                {
+                    textmeshPro.color = new Color32(115, 47, 47, 255);
+                }
+            }
+
+
+        }
+    }
+
+ 
+    IEnumerator GetText()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("http://68.183.206.206:8000/text.json");
+        yield return www.Send();
+
+        if (www.isNetworkError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            //string itemsData = JsonUtility.ToJson(www.downloadHandler.text);
+            // Show results as text
+            items = JSON.Parse(www.downloadHandler.text);
+            //Debug.Log(www.downloadHandler.text);
+            Debug.Log(items["textDatas"][10]["Input"]);
+            Debug.Log(items["textDatas"]);
+
+            // Or retrieve results as binary data
+            //byte[] results = www.downloadHandler.data;
+        }
+    }
+
+}
+
+
